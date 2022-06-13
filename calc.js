2b8aa89944baf5d21afe0ea21cb8864c3506309b
Luan souza <98497673+Luansouza10@users.noreply.github.com>
2022-06-12 23:32:09 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,14 @@
+function Limpar(id1, id2){
+    document.getElemntById(id1).value="";
+    document.getElemntById(id2).value="";
+}
+
+function Calcular(numero1, numero2, operacao, saída) {
+    var num1 = document.getElemntById(numero1).value="";
+    var num2 = document.getElemntById(numero2).value="";
+    var operador = document.getElemntById(operacao).options[document.getElementById(oepracao).selectIndex]
+    var expressao = num1 + operador + num2;
+    resultado = eval(expressao); 
+    document.getElementById("saida").innerHTML = resultado;
+    
+}
